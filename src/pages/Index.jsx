64ad7b0ed2276428473461b7cd5d98690b95dd15
Mike McCune <mike@ziptie.app>
2024-05-27 15:43:41 -0700

    Chart example

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,55 @@
-
 import { withAuthInfo, useRedirectFunctions, useLogoutFunction } from '@propelauth/react'
 import { useColors } from "./colors";
 import { Heading, Text, Box } from "@chakra-ui/react";
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Register the components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Index = withAuthInfo((props) => {
   const { textColor } = useColors();
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <Box>
       <Heading size="xl" mb={4} color={textColor}>
@@ -13,9 +58,9 @@ const Index = withAuthInfo((props) => {
       <Text>
         The good stuff is in the Activity page
       </Text>
+      <Bar data={data} />
     </Box>
   );
-}
-)
+})
 
 export default Index
